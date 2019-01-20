317aacd8fa3ab3a32d0ee8cf32acda7f44d42d7e
Clloud <chenshaofengsf@163.com>
2019-01-20 15:08:52 +0800

    éProject initialization.

---
+++ app/api/v1/user.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+'''
+@author: Cloud
+@time: 2019/1/14 21:42
+'''
+from flask import jsonify, g
+from app.models.user import User
+from app.libs.token_auth import auth
+from app.models.base import db
+from app.libs.error_code import DeleteSuccess
+from . import api
+
+
+@api.route('/user', methods=['GET'])
+@auth.login_required
+def get_user():
+    uid = g.user.uid
+    user = User.query.filter_by(id=uid).first_or_404()
+    user = user.hide('auth')
+    user = user.append('id')
+    return jsonify(user)
+
+
+@api.route('/user', methods=['DELETE'])
+@auth.login_required
+def delete_user():
+    uid = g.user.uid
+    with db.auto_commit():
+        user = User.query.filter_by(id=uid).first_or_404()
+        user.delete()
+    return DeleteSuccess()
