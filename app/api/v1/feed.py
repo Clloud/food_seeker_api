ae9eb8166522a7b4c76c6df7b00db8f48d0eb47b
MiddleWayExplorer <39698406+MiddleWayExplorer@users.noreply.github.com>
2019-03-12 23:11:11 +0800

    Update feed.md

--- app/api/v1/feed.py
+++ app/api/v1/feed.py
@@ -12,18 +12,18 @@ from . import api
 
 @api.route('/feed/restaurants', methods=['GET'])
 def feed_restaurants():
-    restaurants = Restaurant.query.order_by('-grade').custom_paginate()
+    restaurants = Restaurant.filter_by().query.order_by('-grade').custom_paginate()
     return jsonify(restaurants)
 
 
 @api.route('/feed/foods', methods=['GET'])
 def feed_foods():
-    foods = Food.query.order_by('-grade').custom_paginate()
+    foods = Food.query.filter_by().order_by('-grade').custom_paginate()
     return jsonify(foods)
 
 
 @api.route('/feed/reviews', methods=['GET'])
 def feed_reviews():
-    reviews = Review.query.order_by('-create_time').custom_paginate()
+    reviews = Review.query.filter_by().order_by('-create_time').custom_paginate()
     reviews = [review.append('restaurant_id').hide('restaurant') for review in reviews]
     return jsonify(reviews)
