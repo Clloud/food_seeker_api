e7e22f21352cf5d7c89f77d9a06bfeb005f9ce39
Clloud <chenshaofengsf@163.com>
2019-03-12 16:39:03 +0800

    modify user model

--- app/models/user.py
+++ app/models/user.py
@@ -3,8 +3,8 @@
 @author: Cloud
 @time: 2019/1/14 23:57
 '''
+from flask import current_app
 from sqlalchemy import Column, Integer, String, SmallInteger, orm
-from sqlalchemy.orm import relationship
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.models.base import Base, db
 from app.libs.error_code import NotFound, AuthFailed
@@ -17,7 +17,8 @@ class User(Base):
     auth = Column(SmallInteger, default=1)
     _password = Column('password', String(128))
     mobile = Column(String(15), unique=True)
-    avatar_url = Column(String(100))
+    avatar_id = Column(Integer)
+    _avatar_url = Column('avatar_url', String(100))
 
     @orm.reconstructor
     def __init__(self):
@@ -31,6 +32,15 @@ class User(Base):
     @password.setter
     def password(self, raw):
         self._password = generate_password_hash(raw)
+
+    @property
+    def avatar_url(self):
+        return current_app.config['IMAGE_URL_PREFIX'] + self._avatar_url \
+            if self._avatar_url else self._avatar_url
+
+    @avatar_url.setter
+    def avatar_url(self, value):
+        self._avatar_url = value
 
     @staticmethod
     def register_by_email(nickname, account, secret):
